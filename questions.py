e967253c46d02c961881d6fecf27609018b05de8
BartlomiejLesniowski <lesionauka@gmail.com>
2024-12-17 23:05:24 +0100

    Upload questions.py

---
+++ questions.py
@@ -0,0 +1,38 @@
+question = [
+    {
+        'prompt': 'What is the capital of the New York state?',
+        'options': ['A. New York', 'B. Albany', 'C. Manhattan', 'D. Bridgewater'],
+        'answer': 'B',
+        'explanation': 'Albany is the capital of New York state.'
+    },
+    {
+        'prompt': 'What is the largest city of Romania?',
+        'options': ['A. Sibiu', 'B. Budapest', 'C. Ljubljana', 'D. Bucharest'],
+        'answer': 'D',
+        'explanation': 'Bucharest is the largest city of Romania.'
+    },
+    {
+        'prompt': 'Who famously crossed the Alps with elephants on the way to war with the Romans?',
+        'options': ['A. Hannibal', 'B. Napoleon Bonaparte', 'C. Gaius Iulius Caesar', 'D. Alexander the Great'],
+        'answer': 'A',
+        'explanation': 'Hannibal famously crossed the Alps with elephants on the way to war with the Romans.'
+    },
+    {
+        'prompt': 'Which country drinks the most coffee per capita?',
+        'options': ['A. Finland', 'B. Italy', 'C. France', 'D. Spain'],
+        'answer': 'A',
+        'explanation': 'Finland drinks the most coffee per capita.'
+    },
+    {
+        'prompt': 'How many dots appear on a pair of dice?',
+        'options': ['A. 40', 'B. 42', 'C. 44', 'D. 22'],
+        'answer': 'B',
+        'explanation': 'There are 42 dots on a pair of dice.'
+    },
+    {
+        'prompt': 'How much is 5!?',
+        'options': ['A. 15', 'B. 5', 'C. 120', 'D. 25'],
+        'answer': 'C',
+        'explanation': '5! is equal to 120.'
+    }
+]
